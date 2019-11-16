5df70b73ca7706d42c0f9e324576e30f09977a64
exilon2400 <exilon2400@hotmail.com>
2019-11-16 14:23:47 +0100

    Display

--- index.php
+++ index.php
@@ -35,7 +35,7 @@ if ($method == "POST") {
 
     $response = new \stdClass();
     $response->speech = $speech;
-    $response->displayText = "";
+    $response->displayText = $speech;
     $response->source = "webhook";
     echo json_encode($response);
 
